4c7da8c1c55f8418852f27ed50b3fc8a82a92080
setlhako <robertsetlhako@gmail.com>
2026-03-18 10:45:37 -0400

    Task 3 complete - format emotion detection output

--- test_emotion.py
+++ test_emotion.py
@@ -1,6 +1,4 @@
-import json
 from emotion_detection import emotion_detector
 
-result = emotion_detector('I am so happy today')
-formatted = json.loads(result)
-print(json.dumps(formatted, indent=4))
+result = emotion_detector('I am so happy I am doing this')
+print(result)
